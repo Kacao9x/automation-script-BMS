c4c82878cd2b6d878bc24fd10f02449b23512313
Kacao Cao <kacao@iastate.edu>
2018-08-10 16:21:47 -0400

    remove more unneccessary collumns

--- merge_big_cycler.py
+++ merge_big_cycler.py
@@ -55,69 +55,70 @@ def merge_column(table):
     return tb
 
 keyword         = 'cycle'
-path            = r'Me01-H100_180731/'
-cycler_path     = path + 'Cycler_Data_Merc_180731.csv'
-final_log_path  = path + 'test_log_sorted.csv'
+path            = 'Me02-H100_180810/'
+cycler_path     = path + 'Cycler_Data_Merc_180810.csv'
+cycler_path_new = path + 'Cycler_Data_Merc_180810_new.csv'
+final_log_path  = path + 'raw_sorted_logs.csv'
 __PERIOD__  = 5                                                                 #time difference btw each log
 _start_row  = 1                                                                 #number of header to be remove
 
 
 
-# with open(path + 'ME01-H100-073118.txt', 'r') as my_file:
-#     lines = pd.read_csv(my_file, header=3, sep=r'\s\s+', error_bad_lines=False, engine='python')
-#     my_file.close()
-#
-# # cycler_data = lines.iloc[::10, 0:10]
-# cycler_data = lines.iloc[:, 0:10]
-# print (cycler_data.shape)
-#
-#
-# header_list = ['id_num', 'time', 'del', 'current',
-#                      'del2', 'cap(mAh)', 'cap(microAh)', 'en(mWh)',
-#                     'en(microWh)', 'Date/Time']
-# cycler_data.columns = header_list
-# del cycler_data['del'], cycler_data['del2']
-#
-# #added extra 'id' columns to shift the first rows
-# header_list = ['id','id_num', 'time', 'current',
-#                'cap(mAh)', 'cap(microAh)', 'en(mWh)',
-#                'en(microWh)', 'Date/Time']
-# cycler_data = cycler_data.reindex(columns = header_list)
-# cycler_data.to_csv(cycler_path)
-#
-# '''
-# search for rows that need to shift
-# '''
-#
-# ind = []
-# ind = (cycler_data.index[ cycler_data['time'].str.contains('Chg') ].tolist()) \
-#       + (cycler_data.index[ cycler_data['time'].str.contains('Rest') ].tolist())
-# ind.sort()
-# print ind
-#
+with open(path + 'Me02-H100_180810.txt', 'r') as my_file:
+    lines = pd.read_csv(my_file, header=3, sep=r'\s\s+', error_bad_lines=False, engine='python')
+    my_file.close()
+
+# cycler_data = lines.iloc[::10, 0:10]
+cycler_data = lines.iloc[:, 0:10]
+print (cycler_data.shape)
+
+
+header_list = ['id_num', 'time', 'del', 'current',
+                     'del2', 'cap(mAh)', 'cap(microAh)', 'en(mWh)',
+                    'en(microWh)', 'Date/Time']
+cycler_data.columns = header_list
+del cycler_data['del'], cycler_data['del2'], \
+    cycler_data['en(mWh)'], cycler_data['en(microWh)']
+
+#added extra 'id' columns to shift the first rows
+header_list = ['id','id_num', 'time', 'current',
+               'cap(mAh)', 'cap(microAh)', 'Date/Time']
+cycler_data = cycler_data.reindex(columns = header_list)
+cycler_data.to_csv(cycler_path)
+
+'''
+search for rows that need to shift
+'''
+
+ind = []
+ind = (cycler_data.index[ cycler_data['time'].str.contains('Chg') ].tolist()) \
+      + (cycler_data.index[ cycler_data['time'].str.contains('Rest') ].tolist())
+ind.sort()
+print ind
+
 # cycler_data.drop(index=10480)
-#
-# # index = [0, 164141, 368782, 609427, 161139, 365779,606425, 811031]
-# # int = index.sort()
-#
-# # transpose the dataframe for shifting rows
-# cycler_data_t = cycler_data.T
-#
-# for i in ind:
-#     cycler_data_t[i] = cycler_data_t.iloc[:,i].shift(-1).tolist()
-#
-# cycler_data = cycler_data_t.T
-# cycler_data.to_csv(cycler_path)
+
+# index = [0, 164141, 368782, 609427, 161139, 365779,606425, 811031]
+# int = index.sort()
+
+# transpose the dataframe for shifting rows
+cycler_data_t = cycler_data.T
+
+for i in ind:
+    cycler_data_t[i] = cycler_data_t.iloc[:,i].shift(-1).tolist()
+
+cycler_data = cycler_data_t.T
+cycler_data.to_csv(cycler_path)
 
 #------------------------------------------------------------------------------#
-with open(cycler_path) as outfile:
-    cycler_data = pd.read_csv(outfile, sep=',', error_bad_lines=False)
-outfile.close()
+# with open(cycler_path) as outfile:
+#     cycler_data = pd.read_csv(outfile, sep=',', error_bad_lines=False)
+# outfile.close()
 # print(cycler_data.head().to_string())
 
 
 
-cycler_data = merge_column(cycler_data)
+# cycler_data = merge_column(cycler_data)
 
 
 # cycler_data.to_csv(final_log_path)

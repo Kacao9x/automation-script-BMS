f87d14dc3115786717d137f7e32e32d601cba31a
Dell-kacao <kacao@iastate.edu>
2018-09-12 15:38:39 -0400

    find interception in the plot

---
+++ interception.py
@@ -0,0 +1,11 @@
+from scipy.optimize import fsolve
+import pylab
+import numpy
+
+def findIntersection(fun1,fun2,x0):
+    return fsolve(lambda x : fun1(x) - fun2(x),x0)
+
+result = findIntersection(numpy.sin,numpy.cos,0.0)
+x = numpy.linspace(-2,2,50)
+pylab.plot(x,numpy.sin(x),x,numpy.cos(x),result,numpy.sin(result),'ro')
+pylab.show()

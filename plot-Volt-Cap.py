c5740fa70a4e1110235f4895b9b54a9a3626e843
kacao-t400 <kacao@iastate.edu>
2018-09-14 12:28:47 -0400

    hightlight in plot and fix the float division

--- plot-Volt-Cap.py
+++ plot-Volt-Cap.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-
+import matplotlib.pyplot as plt
 import subprocess
 import datetime as dt
 import thLib as th
@@ -40,47 +40,94 @@ def merge_column(table):
     capmAh = table.columns.get_loc("cap(mAh)")
     vol    = table.columns.get_loc("volt")
 
-    for i in range(len(ind) -1 ):
+    # for i in range(len(ind) -1 ):
 
         # Addition of cap for CV and CC cycle (step 1 and 2)
-        if (table.iat[int(ind[i]), id_num] == 'Rest' and
-              table.iat[int(ind[i - 1]), id_num] == 'CCCV_Chg'):
-
-            tot = table.iat[int(ind[i]) - 1, capmAh]
-            diff = int(ind[i + 1]) - int(ind[i])
-            for j in range(diff):
-                table.iat[int(ind[i]) + j, capmAh] = float(
-                    table.iat[int(ind[i]) + j, capmAh]/tot)
+        # if (table.iat[int(ind[i]), id_num] == 'Rest' and
+        #       table.iat[int(ind[i - 1]), id_num] == 'CCCV_Chg'):
+        #
+        #     tot = table.iat[int(ind[i]) - 1, capmAh]
+        #     print('i: %s tot: %s' % (str(tot), str(i)))
+        #     diff = int(ind[i + 1]) - int(ind[i])
+        #     for j in range(diff):
+        #         table.iat[int(ind[i]) + j, capmAh] = float(
+        #             table.iat[int(ind[i]) + j, capmAh]/tot)
 
         # Subtraction the capacity for dischage cycle
-        elif (table.iat[int(ind[i + 1]), id_num] == 'Rest' and
-              table.iat[int(ind[i]), id_num] == 'CC_DChg'):
+        # if (table.iat[int(ind[i + 1]), id_num] == 'Rest' and
+        #       table.iat[int(ind[i]), id_num] == 'CC_DChg'):
+        #
+        #     tot = table.iat[int(ind[i]) - 1, capmAh]
+        #     tot = 55.1067
+        #     print ('i: %s tot: %s' % (str(tot), str(i)) )
+        #     diff = int(ind[i + 1]) - int(ind[i])
+        #     for j in range(diff):
+        #         table.iat[int(ind[i]) + j, capmAh] = float(
+        #             table.iat[int(ind[i]) + j, capmAh] / tot)
 
-            tot = table.iat[int(ind[i]) - 1, capmAh]
-            diff = int(ind[i + 1]) - int(ind[i])
-            for j in range(diff):
-                table.iat[int(ind[i]) + j, capmAh] = float(
-                    table.iat[int(ind[i]) + j, capmAh] / tot)
+    tot = 55.193
+    diff = 3259 - 0
+    for j in range(diff):
+        table.iat[ j, capmAh] = float(table.iat[j,capmAh] / tot)
 
+    tot = 55.1067
+    # print('i: %s tot: %s' % (str(tot), str(i)))
+    diff = 5968 - 3321
+    for j in range(diff):
+        table.iat[3321 + j, capmAh] = float(table.iat[3321 + j,
+                                                   capmAh] / tot)
     return table
 
 
 
 def main():
-    with open(cycler_path) as outfile:
+    with open(cycler_path_new) as outfile:
         table = pd.read_csv(outfile, sep=',', error_bad_lines=False)
     outfile.close()
 
 
-    NAN_finder = table['id'].notna()  # a boolean list of ID column
-    global ind
-    for i in range(len(NAN_finder)):
-        if NAN_finder[i] == True:
-            ind = np.append(ind, i)
-    print (ind)
+    # NAN_finder = table['id'].notna()  # a boolean list of ID column
+    # global ind
+    # for i in range(len(NAN_finder)):
+    #     if NAN_finder[i] == True:
+    #         ind = np.append(ind, i)
+    # print (ind)
+    #
+    # table = merge_column(table)  # Merge capactity of CC and CV stages
+    # table.to_csv(cycler_path_new)
 
-    table = merge_column(table)  # Merge capactity of CC and CV stages
-    table.to_csv(cycler_path_new)
+    vol1,vol2,amp1,amp2 = [],[],[],[]
+    amp1 = [num / 55.193 for num in amp1]
+    amp2 = [num / 55.1067 for num in amp2]
+
+    for i in range (1, 3259):
+        #plot CCCV
+        vol1.append(table['volt'][i])
+        amp1.append(table['cap(mAh)'][i])
+    plt.figure(1)
+    plt.scatter(amp1, vol1)
+    plt.title('Capacity vs Voltage in Charge cycle')
+    plt.xlabel('Cap %')
+    plt.ylabel('Volt (V)')
+    plt.axvspan(0.095, 0.105, color='red', alpha=0.5)
+    plt.interactive(True)
+    plt.show()
+
+
+    for j in range (3322, 5968):
+        #plot Discharge
+        # plot CCCV
+        vol2.append(table['volt'][j])
+        amp2.append(table['cap(mAh)'][j])
+    plt.figure(2)
+    plt.scatter(amp2, vol2)
+    plt.title('Capacity vs Voltage in Discharge cycle')
+    plt.xlabel('Cap %')
+    plt.ylabel('Volt (V)')
+    plt.axvspan(0.895, 0.905, color='red', alpha=0.5)
+    plt.interactive(False)
+    plt.show()
+
 
     # find the CC-CV charge/discharge ID_num
     # ind_charge, ind_discharge = [], []

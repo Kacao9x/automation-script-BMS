0d5180a8d12dd37876b8b3e0a56ba2d31d313e9e
kacao <kacao@iastate.edu>
2019-02-27 11:25:38 -0500

    new sketch to upload post-test data to mongo

--- mongo_try.py
+++ mongo_try.py
@@ -1,8 +1,14 @@
-from lib.echoes_protocol import *
-from lib.echoes_database import *
+from datetime import time
+import pandas as pd
 import subprocess
+from pprint import pprint
+from bson import ObjectId
+import json
 
-#=============================================================================#
+
+from lib.echoes_database import *
+
+
 
 #Subprocess's call command with piped output and active shell
 def Call(cmd):
@@ -18,7 +24,7 @@ def Popen(cmd):
 def PopenIter(cmd):
     return subprocess.Popen(cmd, stdout=subprocess.PIPE,
                             shell=True).stdout.readline
-#=============================================================================#
+#==============================================================================#
 
 # display the file with keyword in ascending using BASH
 def display_list_of_file(key):
@@ -31,109 +37,302 @@ def display_list_of_file(key):
     return list_name
 
 
-def _get_timestamp_from_filename( filename ):
+def concat_all_data(tempC = bool, search_key = str):
+    '''
+    :param cycle: keyword number to search and sort out
+    :param tempC: True to read the temperature files, False otherwise
+    :return: a dataframe contains all avg capture in a custom format
+            an array of all data sets
+    '''
+    big_set = pd.DataFrame()
+    global echoes_index
+    echoes_index = []
+    if tempC:
+        ''' Read the temperature files
+        '''
+        tC_1, tC_2 = [], []
+        list_file = display_list_of_file(search_key)
+        print (list_file)
+        for filename in list_file:
 
-    i = filename.split('-')
-    # cycle1 - raw - 0 - 2018 - 08 - 31 - 16 - 44 - 34 - echoes - d
-    if i[1] == 'temp':
-        endtime = '2018' + '-' + i[3] + '-' + i[4] + ' ' \
-                  + i[5] + ':' + i[6] + ':' + i[7]
-        print ('endtime raw: ' + endtime)
+            with open(address + filename) as my_file:
+                y_str = my_file.read()
+                y_str = y_str.splitlines()
+            my_file.close()
+
+            for i, num in enumerate(y_str):
+                if len(num.split()) > 2:
+                    tC_1.append(num.split()[1])
+                    tC_2.append(num.split()[2])
+
+        return tC_2, tC_1
+
 
     else:
-        endtime = '2018' + '-' + i[4] + '-' + i[5] + ' ' \
-                  + i[6] + ':' + i[7] + ':' + i[8]
-        print ('endtime filtered: ' + endtime)
+        '''Read data from capture files
+        '''
+        list_file = display_list_of_file(search_key)
+        print (list_file)
+        for captureID, filename in enumerate(list_file):
 
-    return endtime
+            with open(address + filename) as my_file:
+                y_str = my_file.read()
+                y_str = y_str.splitlines()
+            my_file.close()
+
+            data = [float(num) for num in y_str]                                # convert string to float
+
+            single_set = pd.DataFrame({captureID: data})                        # concat all data set into a singl dataframe
+            big_set = pd.concat([big_set, single_set], axis=1,
+                                ignore_index=True)
+
+        big_set = big_set.fillna(0)                                             # with 0s rather than NaNs
+
+        return big_set, list_file
 
 
-def read_pickle():
-    record = echoes_1.getSessionData()
-    print ('impulseVoltage: ' + str(record['impulseVoltage']))
-    print ('impulseType: ' + str(record['impulseType']))
-    print ('vga Gain: ' + str(record['vgaGain']))
-    # print ('impulseCycle: ' + str(record['impulseCycles']))
-    # print ('capture ADC: ' + str(record['captureAdc']))
-    # print ('adc Sync Delay: ' + str(record['adcSynchroDelay']))
+def _get_timestamp( filename ):
+    timest = {}
+    i = filename.split('-')
+
+    (timest['year'], timest['month'], timest['day'])    = int(i[2]), int(i[3]), int(i[4])
+    (timest['hour'], timest['min'], timest['sec'])      = int(i[5]), int(i[6]), int(i[7])
+
+
+    return timest
+
+
+def post_csv_report():
+
+    global bucket
+    with open(address + filename) as outfile:
+        table = pd.read_csv(outfile, sep=',', error_bad_lines=False)
+    outfile.close()
+
+    print (len(table.index))
+
+    cycle = len(table.index)
+    cycle_id = 70
+
+    while cycle_id < cycle + 1:
+
+        # if cycle_id == 127:
+        #     cycle_id = 128
+        row = cycle_id - 1                                                      # row in PANDAS table start from 0
+        print ('row %s' % str(row))
+
+        res = echoes_db.search(query={'test_apparatus.battery_id': battery_id,
+                                      'test_results.capture_number': cycle_id,
+                                      'test_setting.input_channel': input_channel},
+                               collection=cabinet)
+
+        for post in res:
+            pprint(post['_id'])
+            data = list(table.iloc[row, -512 : -1].values)                      # retrieve signal data from the report
+
+            # post['test_results']['average'] = data
+            post['test_results'].update({
+                'temperature'   : {
+                    'top'   : float(table['Temperature_top'][cycle_id -1]),
+                    'bottom': float(table['Temperature_bottom'][cycle_id -1])
+                },
+                'cap(mAh)'      : float(table['cap(mAh)'][cycle_id -1]),
+                'current'       : float(table['current'][cycle_id -1]),
+                'volt'          : float(table['volt'][cycle_id -1]),
+                'charging'      : int(table['charging'][cycle_id -1]),
+                'SoH'           : float(table['SoH'][cycle_id - 1]),
+                'average'       : data
+            })
+            echoes_db.update(post, {'_id': post['_id']},
+                             collection=cabinet)
+
+            # echoes_db.delete({'test_results.average':[]},
+            #                  collection='tuna-can-sample')
+
+        cycle_id += 1                                                           # move to next row
+
+
+    print('completed upserting AVG data')
 
     return
 
 
-def main(packet=None):
+def post_raw_data():
+    global bucket
 
-    read_pickle()
+    with open(address + filename) as outfile:
+        table = pd.read_csv(outfile, sep=',', error_bad_lines=False)
+    outfile.close()
 
-    packet = {}
-    cycle = 10
-    avgNum = 60
-    amp, tC = [], []
-    for cycle_num in range(cycle):
+    cycle = 200
+    cycle_id = 70
 
-        filelist = display_list_of_file('cycle' + str(cycle_num + 1) + '-')
-        print (filelist)
+    while cycle_id < cycle + 1:
+        bucket = {}
 
-        for avg_num, name in enumerate( filelist ):
-            #read the amplitude value
-            with open( address + name ) as readout:
-                y_str = readout.read()
-                y_str = y_str.splitlines()
-                amp = []
-                for j, num in enumerate(y_str):
-                    if j < len(y_str) - 1:
-                        amp.append(float(num))
-                    else:
-                        if len(num.split()) > 2:
-                            temp = num.rstrip().split('Temperature:')[1]
-                            temp = temp.split('oC')[0]
-                            tC.append(float(temp))
-                        else:
-                            amp.append(float(num))
-            readout.close()
+        # if cycle_id == 127:
+        #     cycle_id = 128
+
+        timest = _get_timestamp(table['FileName'][cycle_id - 1])
+        bucket['timestamp'] = datetime.datetime(timest['year'], timest['month'],
+                                                timest['day'], timest['hour'],
+                                                timest['min'], timest['sec'])
+
+        # bucket['test_apparatus'] = {
+        #     'battery_id'    : battery_id,
+        #     'transducer_id' : 67143,
+        #     'echoes_id'     : 'echoes-a'
+        # }
+
+        bucket['battery_id']    = battery_id
+        bucket['transducer_id'] = 67143
+        bucket['echoes_id']     = 'echoes-a'
+
+        # bucket['project_name']  = project
+        bucket['test_examiner'] = examiner
+
+        bucket['test_setting']  = {
+            'impulse_volt'  : 85,
+            'vga_gain'      : 0.55,
+            'delay_ms'      : 25,
+            'sampling_rate' : 7200000,
+            'impulse_type'  : 'neg-bipolar',
+            'input_channel' : input_channel
+        }
+
+        row = cycle_id - 1                                                  # row in PANDAS table start from 0
+        print ('row %s' % str(row))
+        data = list(table.iloc[row, -512 : -1].values)                      # retrieve signal data from the report
+
+        bucket['test_results']  = {
+            'temperature'   : {
+                    'top'   : float(table['Temperature_top'][cycle_id -1]),
+                    'bottom': float(table['Temperature_bottom'][cycle_id -1])
+            },
+            'cap(mAh)'      : float(table['cap(mAh)'][cycle_id -1]),
+            'current'       : float(table['current'][cycle_id -1]),
+            'volt'          : float(table['volt'][cycle_id -1]),
+            'charging'      : int(table['charging'][cycle_id -1])
+        }
+
+        bucket['capture_number']= cycle_id,
+        bucket['average']       = data
+        # bucket['raw_data']      = []
+        bucket['SoH']           = float(table['SoH'][cycle_id - 1])
+
+        
+        ''' Encapsulate the RAW DATA into bucket '''
+        oneRead, list_file = concat_all_data(tempC=False, search_key='cycle' +
+                                            str(cycle_id) + '-')
+
+        [row, column] = oneRead.shape
+        print (column)
+
+        avgPos = 1
+        while avgPos < column + 1:
+            value = list(oneRead.loc[:, avgPos - 1].values)
+
+            # bucket['raw_data'].append({'run': avgPos, 'result': value})
+            field_name = 'raw_data_' + str(avgPos)
+            bucket[field_name] = value
+            avgPos += 1  # go to next column
 
 
-            record = echoes_1.getSessionData()
-            # record['capture_data'] = amp
-            record['session'] = 'Me02-H100_180914'
+        res = echoes_db.insert_capture(record=bucket, collection=cabinet)
+        print (res)
+        print ("completed uploading cycle %s" % str(cycle_id))
+        cycle_id += 1
 
-            record['test_results'] = {}
-            record['test_results']['data'] = amp
-            record['test_results']['cycle_number']  = cycle_num + 1
-            record['test_results']['avg_number']    = avg_num
-            record['test_results']['name']          = name
-            # packet['test_setting']['impulseVoltage']= record['impulseVoltage']
-            # packet['test_setting']['impulseType']   = record['impulseType']
-            # packet['test_setting']['vgaGain']       = record['vgaGain']
 
-            # packet['test_result']['data']           = amp
-            # packet['test_result']['session']        = 'Me02-H100'
-            # packet['test_result']['cycle_number']   = cycle_num + 1
-            # packet['test_result']['avg_number']     = avg_num
-            # packet['test_result']['source_filename']= name
+        #------- upsert every run each capture into 'test-results' --------#
+        # bucket = {
+        #   dat : [
+        #       {"id": 110, "data": {"Country": "ES", "Count": [64,65,66]}},
+        #       {"id": 112, "data": {"Country": "ES", "Count": 5}},
+        #       {"id": 114, "data": {"Country": "UK", "Count": 3}}
+        #   ]
+        #}
+        # db.test_collection.find({"data.Country": "ES"})
+        # db.test_collection.find({"data.Count": {"$lt": 6}})
 
-            echoes_db.insert_capture(record)
 
-    print("time:" + str(datetime.datetime.now()))
+    return
+
+
+#==============================================================================#
+
+# battery_id      = 'TC02-H75'
+battery_id      = 'TC16'    #input('Input Battery ID: \n')
+SoH             = '73'   #input('Input SoH value: \n')
+date            = '181219'  #input('Testing date: \n')
+    
+input_channel   = 'secondary'
+cabinet         = 'tuna-sample'
+examiner        = 'Khoi'
+# project         = 'TUNA016-Phase1-Build_ML_Model'
+
+
+print("Initializing database")
+echoes_db       = database(database='echoes-captures')
+echoes_db.mongo_db = cabinet
+
+
+filename = battery_id + '-H' + str(SoH) + '_181219_' + input_channel + '-sorted.csv'
+bucket = {}
+address = '/media/kacao/Ultra-Fit/titan-echo-boards/Echo-A/TC16-H73_181219/' + input_channel + '/'
+
+
+if __name__ == '__main__':
+
+    post_raw_data()
+    # post_csv_report()
+
+    # echoes_db.createIndex( [( 'battery_id', pymongo.ASCENDING )],
+    #                         unique=False, collection=cabinet)
+    echoes_db.createIndex( [( 'capture_number', pymongo.ASCENDING )],
+                            unique=False, collection=cabinet)
+    # echoes_db.createIndex( [( 'SoH', pymongo.ASCENDING )],
+    #                         unique=False, collection=cabinet)
+
+    # res = echoes_db.search(query={'test_apparatus.battery_id': battery_id,
+    #                               'test_results.capture_number': 71,
+    #                               'test_setting.input_channel': input_channel},
+    #                        collection=cabinet)
+
+    # res_2 = echoes_db.find_one(query={'test_apparatus.battery_id': battery_id,
+    #                                   'test_results.capture_number': 71,
+    #                                   'test_setting.input_channel': input_channel},
+    #                            collection=cabinet)
+
+
+
+
+    # pprint(res_2['test_results']['raw_data'][0]['result'])
+
+    # echoes_db.delete( query={'test_apparatus.battery_id': battery_id},
+    #                            collection=cabinet)
+
+    # for num in range(0, 200):
+    #     echoes_db.delete( query={'test_apparatus.battery_id': battery_id,
+    #                                   'test_results.capture_number': num},
+    #                            collection=cabinet)
+
+
+    # pprint(res_2)
+    # pprint(res)
+    # for post in res:
+    #     pprint(post)
+
+    # echoes_db.delete(query={'test_apparatus.battery_id': battery_id,
+    #                               'test_results.capture_number': 46,
+    #                               'test_setting.input_channel': input_channel},
+    #                        collection=cabinet)
+
+
+    # for run in res_2['test_results']['raw_data']:
+    #     if run['run'] == 60:
+    #         pprint(res_2['test_results']['temperature'])
 
     echoes_db.close()
 
-    return
-
-
-
-print("Initializing EchOES")
-
-echoes_1 = echoes()
-echoes_1.set_impulse_type(Impulse_Type.half)
-echoes_1.set_impulse_type(Impulse_Voltage.impulse_85v)
-echoes_1.set_vga_gain(0.55)
-
-print("Initializing database")
-echoes_db = database()
-echoes_db.mongo_db = 'echoes-captures'
-
-address = 'data/'                           # prompts user to select folder
-
-if __name__ == '__main__':
-    main()
 
